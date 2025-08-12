8840decfd01f42a06ba88683625f06b17296f869
LeonardoSilva <leonardosilva9737@gmail.com>
2025-08-12 19:26:09 +0000

    aula 03

---
+++ aula03/exercicios.py
@@ -0,0 +1,63 @@
+#exercicio01
+print("Olá Mundo!!")
+#exercicio02
+print("Olá ")
+print("Mundo")
+print("!!")
+#exercicio03
+print("Nome : João")
+print("Idade: 20 anos")
+print("Curso: Python Básico")
+#exercicio04
+nome = "João"
+idade= "20 anos"
+curso= "Python Básico"
+print("Nome :",nome,"Idade:",idade,"Curso:",curso)
+#exercicio05
+print("##############################################################")
+print("#                                                            #")
+print("#                                                            #")
+print("#                                                            #")
+print("#                                                            #")
+print("#                                                            #")
+print("#                                                            #")
+print("#                                                            #")
+print("#                                                            #")
+print("#                                                            #")
+print("#                                                            #")
+print("#                                                            #")
+print("#                                                            #")
+print("#                                                            #")
+print("##############################################################")
+#exercicio06
+a = 10
+b = 3
+print("a = 10")
+print("b = 3")
+print("Soma", a+b)
+print("Subtração", a-b)
+print("Multiplicação", a*b)
+print("Divisão", a/b)
+print("Divisão inteira", a//b)
+print("Resto da divisão", a%b)
+print("Potência", a**b)
+#exercicio07
+n1 = int((input("Informe um numero: ")))
+n2 = int((input("Informe um numero: ")))
+print("Soma", n1+n2)
+print("Subtração", n1-n2)
+print("Multiplicação", n1*n2)
+print("Divisão", n1/n2)
+#exercicio08
+idade = int((input("Informe sua idade: ")))
+dias  = idade*365
+bisexto = idade//4
+print("Você viveu por ", dias+bisexto, " dias")
+#exercicio09
+altura  = int((input("Informe a altura  : ")))
+largura = int((input("Informe a largura : ")))
+print("A area do retangulo é ", altura*largura)
+#exercicio10
+valor_da_compra  = int((input("Informe o valor da compra : ")))
+valor_pago       = int((input("Informe o valor pago      : ")))
+print("O valor do troco é : ", valor_pago - valor_da_compra)
